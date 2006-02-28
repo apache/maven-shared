9262ed71147d72289dad40035714b3a7ee995846
John Dennis Casey <jdcasey@apache.org>
2006-02-28 19:51:36 +0000

    Adding download manager that uses wagons to download arbitrary URLs, and removing the dependency on maven-shared-monitor until we can find a good logging impl.
    git-svn-id: https://svn.apache.org/repos/asf/maven/shared/trunk/maven-shared-io@381764 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/maven/shared/io/DownloadFailedException.java
@@ -0,0 +1,28 @@
+package org.apache.maven.shared.io;
+
+public class DownloadFailedException
+    extends Exception
+{
+    
+    private static final long serialVersionUID = 1L;
+    
+    private String url;
+
+    public DownloadFailedException( String url, String message, Throwable cause )
+    {
+        super( message, cause );
+        this.url = url;
+    }
+
+    public DownloadFailedException( String url, String message )
+    {
+        super( message );
+        this.url = url;
+    }
+    
+    public String getUrl()
+    {
+        return url;
+    }
+
+}

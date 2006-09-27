d6f9927bdf9d7dc8edf5078c9a2f0c9be44bb94d
Jason van Zyl <jvanzyl@apache.org>
2006-09-27 13:12:32 +0000

    Checking in some stubs while Milos and I are working through some examples.
    git-svn-id: https://svn.apache.org/repos/asf/maven/shared/trunk@450429 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-toolchain/src/main/java/org/apache/maven/toolchain/java/sun/AbstractSunJavaToolChain.java
@@ -0,0 +1,11 @@
+package org.apache.maven.toolchain.java.sun;
+
+import org.apache.maven.toolchain.java.AbstractJavaToolChain;
+
+/**
+ * @author Jason van Zyl
+ */
+public abstract class AbstractSunJavaToolChain
+    extends AbstractJavaToolChain
+{
+}

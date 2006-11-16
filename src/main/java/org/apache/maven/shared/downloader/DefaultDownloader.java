961be5a730831005e6e3288e3ac3d1a23e4c4648
Jason van Zyl <jvanzyl@apache.org>
2006-11-16 22:18:14 +0000

    o super simple interface for downloading a single artifact
    git-svn-id: https://svn.apache.org/repos/asf/maven/shared/trunk/maven-downloader@475949 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/maven/shared/downloader/DefaultDownloader.java
@@ -0,0 +1,61 @@
+package org.apache.maven.shared.downloader;
+
+import org.apache.maven.artifact.resolver.ArtifactResolver;
+import org.apache.maven.artifact.resolver.ArtifactResolutionException;
+import org.apache.maven.artifact.resolver.ArtifactNotFoundException;
+import org.apache.maven.artifact.factory.ArtifactFactory;
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.artifact.repository.ArtifactRepository;
+
+import java.util.List;
+import java.io.File;
+
+/**
+ * @author Jason van Zyl
+ * @plexus.component
+ */
+public class DefaultDownloader
+    implements Downloader
+{
+    private ArtifactResolver artifactResolver;
+
+    private ArtifactFactory artifactFactory;
+
+    public File download( String groupId,
+                          String artifactId,
+                          String version,
+                          File localRepository,
+                          String[] remoteRepositories )
+        throws DownloadException, DownloadNotFoundException
+
+    {
+        return download( groupId, artifactId, version, localRepository, remoteRepositories );
+    }
+
+    public File download( String groupId,
+                          String artifactId,
+                          String version,
+                          ArtifactRepository localRepository,
+                          List remoteRepositories )
+        throws DownloadException, DownloadNotFoundException
+
+    {
+        Artifact artifact =
+            artifactFactory.createArtifact( groupId, artifactId, version, Artifact.SCOPE_RUNTIME, "jar" );
+
+        try
+        {
+            artifactResolver.resolve( artifact, remoteRepositories, localRepository );
+        }
+        catch ( ArtifactResolutionException e )
+        {
+            throw new DownloadException( "Error downloading.", e );
+        }
+        catch ( ArtifactNotFoundException e )
+        {
+            throw new DownloadNotFoundException( "Requested download does not exist.", e );
+        }
+
+        return artifact.getFile();
+    }
+}

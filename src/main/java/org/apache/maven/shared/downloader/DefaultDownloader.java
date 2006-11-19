12a3209dd194183a1a4720ff3841909d26adb713
Jason van Zyl <jvanzyl@apache.org>
2006-11-19 22:38:23 +0000

    git-svn-id: https://svn.apache.org/repos/asf/maven/shared/trunk/maven-downloader@476960 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/maven/shared/downloader/DefaultDownloader.java
+++ src/main/java/org/apache/maven/shared/downloader/DefaultDownloader.java
@@ -1,14 +1,14 @@
 package org.apache.maven.shared.downloader;
 
-import org.apache.maven.artifact.resolver.ArtifactResolver;
-import org.apache.maven.artifact.resolver.ArtifactResolutionException;
-import org.apache.maven.artifact.resolver.ArtifactNotFoundException;
-import org.apache.maven.artifact.factory.ArtifactFactory;
 import org.apache.maven.artifact.Artifact;
+import org.apache.maven.artifact.factory.ArtifactFactory;
 import org.apache.maven.artifact.repository.ArtifactRepository;
+import org.apache.maven.artifact.resolver.ArtifactNotFoundException;
+import org.apache.maven.artifact.resolver.ArtifactResolutionException;
+import org.apache.maven.artifact.resolver.ArtifactResolver;
 
-import java.util.List;
 import java.io.File;
+import java.util.List;
 
 /**
  * @author Jason van Zyl

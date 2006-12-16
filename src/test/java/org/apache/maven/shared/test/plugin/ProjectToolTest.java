a01dba4b112995e0032682f380eb5de445ff3fc5
Fabrizio Giustina <fgiust@apache.org>
2006-12-16 15:26:44 +0000

    fix a failing test on windows (path separator), move to a released version of maven-invoker
    git-svn-id: https://svn.apache.org/repos/asf/maven/sandbox/maven-plugin-testing-tools@487810 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/maven/shared/test/plugin/ProjectToolTest.java
+++ src/test/java/org/apache/maven/shared/test/plugin/ProjectToolTest.java
@@ -6,6 +6,7 @@ import org.apache.maven.project.MavenProject;
 import org.apache.maven.project.artifact.ProjectArtifactMetadata;
 import org.apache.maven.shared.test.plugin.ProjectTool.PomInfo;
 import org.codehaus.plexus.PlexusTestCase;
+import org.codehaus.plexus.util.StringUtils;
 
 import java.io.File;
 import java.util.Collection;
@@ -30,14 +31,16 @@ public class ProjectToolTest
     public void testPackageProjectArtifact_ShouldPopulateArtifactFileWithJarLocation() throws Exception
     {
         ProjectTool tool = (ProjectTool) lookup( ProjectTool.ROLE, "default" );
-        
-        File pomFile = new File( "pom.xml" );
-        
-        MavenProject project = tool.packageProjectArtifact( pomFile, "test", true );
-        
-        String path = "target/maven-plugin-testing-tools-test.jar";
 
-        assertEquals( path, project.getArtifact().getFile().getPath() );
+        File pomFile = new File( "pom.xml" );
+
+        MavenProject project = tool.packageProjectArtifact( pomFile, "test", true );
+
+        String expectedPath = "target/maven-plugin-testing-tools-test.jar";
+        // be nice with windows
+        String actualPath = StringUtils.replace( project.getArtifact().getFile().getPath(), "\\", "/" );
+
+        assertEquals( expectedPath, actualPath );
     }
     
     public void testPackageProjectArtifact_ShouldPopulateWithCorrectArtifactAndMetadata() throws Exception

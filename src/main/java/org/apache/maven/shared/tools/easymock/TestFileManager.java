d51a47f842fa6d6102ad56829b80e1d693b8f80a
Fabrizio Giustina <fgiust@apache.org>
2006-12-16 15:59:59 +0000

    fixed license headers (+ some formatting): no functional changes in this commit
    git-svn-id: https://svn.apache.org/repos/asf/maven/sandbox/maven-test-tools@487820 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/maven/shared/tools/easymock/TestFileManager.java
+++ src/main/java/org/apache/maven/shared/tools/easymock/TestFileManager.java
@@ -1,7 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.maven.shared.tools.easymock;
-
-import org.codehaus.plexus.util.FileUtils;
-import org.codehaus.plexus.util.IOUtil;
 
 import java.io.File;
 import java.io.FileReader;
@@ -14,6 +29,9 @@ import java.util.Iterator;
 import java.util.List;
 
 import junit.framework.Assert;
+
+import org.codehaus.plexus.util.FileUtils;
+import org.codehaus.plexus.util.IOUtil;
 
 public class TestFileManager
 {
@@ -106,7 +124,7 @@ public class TestFileManager
     {
         for ( Iterator it = filesToDelete.iterator(); it.hasNext(); )
         {
-            File file = ( File ) it.next();
+            File file = (File) it.next();
 
             if ( file.exists() )
             {
--- src/main/java/org/apache/maven/shared/tools/easymock/TestUtils.java
+++ src/main/java/org/apache/maven/shared/tools/easymock/TestUtils.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.apache.maven.shared.tools.easymock;
 
 import java.io.BufferedReader;
@@ -31,25 +49,26 @@ public final class TestUtils
             IOUtil.close( fw );
         }
     }
-    
-    public static String readFile( File file ) throws IOException
+
+    public static String readFile( File file )
+        throws IOException
     {
         StringBuffer buffer = new StringBuffer();
-        
+
         BufferedReader reader = new BufferedReader( new FileReader( file ) );
-        
+
         String line = null;
-        
-        while( ( line = reader.readLine() ) != null )
+
+        while ( ( line = reader.readLine() ) != null )
         {
             if ( buffer.length() > 0 )
             {
                 buffer.append( '\n' );
             }
-            
+
             buffer.append( line );
         }
-        
+
         return buffer.toString();
     }
 
@@ -57,9 +76,9 @@ public final class TestUtils
     {
         StringWriter sw = new StringWriter();
         PrintWriter pw = new PrintWriter( sw );
-        
+
         error.printStackTrace( pw );
-        
+
         return sw.toString();
     }
 }

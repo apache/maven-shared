16f7c1e4cbaca96d84893e404dc654e193ded49f
Dennis Lundberg <dennisl@apache.org>
2007-03-10 23:14:14 +0000

    [maven-scm] copy for tag maven-model-converter-2.1
    git-svn-id: https://svn.apache.org/repos/asf/maven/shared/tags/maven-model-converter-2.1@516808 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/maven/model/converter/relocators/PluginRelocatorManager.java
+++ src/main/java/org/apache/maven/model/converter/relocators/PluginRelocatorManager.java
@@ -1,7 +1,5 @@
 package org.apache.maven.model.converter.relocators;
 
-import java.util.Collection;
-
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -20,6 +18,8 @@ import java.util.Collection;
  * specific language governing permissions and limitations
  * under the License.
  */
+
+import java.util.Collection;
 
 /**
  * A manager for plugin relocators.

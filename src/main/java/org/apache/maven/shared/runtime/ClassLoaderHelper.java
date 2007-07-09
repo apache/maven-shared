9b4891de3b0be029705b4176ce3ef4536f43c275
Mark Hobson <markh@apache.org>
2007-07-09 22:25:59 +0000

    o Made ClassLoaderHelper ignore unknown URLs o Started ClassLoaderHelperTest
    git-svn-id: https://svn.apache.org/repos/asf/maven/sandbox/trunk/shared/maven-runtime@554776 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/maven/shared/runtime/ClassLoaderHelper.java
+++ src/main/java/org/apache/maven/shared/runtime/ClassLoaderHelper.java
@@ -144,10 +144,6 @@ public class ClassLoaderHelper
         {
             visitProjectsInJar( visitor, url );
         }
-        else
-        {
-            throw new MavenRuntimeException( "Unsupported URL: " + url );
-        }
     }
 
     /**
---
+++ src/test/java/org/apache/maven/shared/runtime/ClassLoaderHelperTest.java
@@ -0,0 +1,102 @@
+package org.apache.maven.shared.runtime;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
+
+import junit.framework.TestCase;
+
+import org.easymock.EasyMock;
+import org.easymock.IMocksControl;
+
+/**
+ * Tests <code>ClassLoaderHelper</code>.
+ * 
+ * @author <a href="mailto:markhobson@gmail.com">Mark Hobson</a>
+ * @version $Id$
+ * @see ClassLoaderHelper
+ */
+public class ClassLoaderHelperTest extends TestCase
+{
+    // fields -----------------------------------------------------------------
+    
+    private IMocksControl mockVisitorControl;
+    
+    private MavenRuntimeVisitor mockVisitor;
+    
+    // TestCase methods -------------------------------------------------------
+    
+    /*
+     * @see junit.framework.TestCase#setUp()
+     */
+    protected void setUp() throws Exception
+    {
+        mockVisitorControl = EasyMock.createStrictControl();
+        mockVisitor = (MavenRuntimeVisitor) mockVisitorControl.createMock( MavenRuntimeVisitor.class );
+        
+        mockVisitorControl.replay();
+    }
+    
+    /*
+     * @see junit.framework.TestCase#tearDown()
+     */
+    protected void tearDown() throws Exception
+    {
+        mockVisitorControl.verify();
+    }
+    
+    // tests ------------------------------------------------------------------
+    
+    public void testWithEmptyJar() throws IOException, MavenRuntimeException
+    {
+        accept( createTempFile( "file", ".jar" ) );
+    }
+    
+    public void testWithUnknownFileExtension() throws IOException, MavenRuntimeException
+    {
+        accept( createTempFile( "file", ".unknown" ) );
+    }
+    
+    // private methods -------------------------------------------------------
+    
+    private URL createTempFile( String prefix, String suffix ) throws IOException
+    {
+        File file = File.createTempFile( prefix, suffix );
+        file.deleteOnExit();
+        
+        return file.toURI().toURL();
+    }
+    
+    private void accept( URL url ) throws MavenRuntimeException
+    {
+        accept( new URL[] {url} );
+    }
+    
+    private void accept( URL[] urls ) throws MavenRuntimeException
+    {
+        ClassLoader classLoader = new URLClassLoader(urls, null);
+        ClassLoaderHelper helper = new ClassLoaderHelper(classLoader);
+ 
+        helper.accept( mockVisitor );
+    }
+}

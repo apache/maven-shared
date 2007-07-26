6a4c9606c3e97de320ef6770e2280cbf725f2b32
John Dennis Casey <jdcasey@apache.org>
2007-07-26 22:42:00 +0000

    git-svn-id: https://svn.apache.org/repos/asf/maven/shared/trunk/maven-shared-io@560022 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/maven/shared/io/location/Location.java
+++ src/main/java/org/apache/maven/shared/io/location/Location.java
@@ -2,21 +2,24 @@ package org.apache.maven.shared.io.location;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.ByteBuffer;
 
 public interface Location
 {
-    
+
     File getFile() throws IOException;
-    
+
     void open() throws IOException;
-    
+
     void close();
-    
+
     int read( ByteBuffer buffer ) throws IOException;
-    
+
     int read( byte[] buffer ) throws IOException;
-    
+
+    InputStream getInputStream() throws IOException;
+
     String getSpecification();
 
 }

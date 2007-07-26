b701762cbd4fa2c82c5ff11d9a970fffc1040747
John Dennis Casey <jdcasey@apache.org>
2007-07-26 22:42:00 +0000

    git-svn-id: https://svn.apache.org/repos/asf/maven/shared/trunk/maven-shared-io@560022 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/maven/shared/io/location/FileLocationTest.java
+++ src/test/java/org/apache/maven/shared/io/location/FileLocationTest.java
@@ -1,129 +1,152 @@
 package org.apache.maven.shared.io.location;
 
+import org.apache.maven.shared.io.TestUtils;
+import org.codehaus.plexus.util.IOUtil;
+
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringWriter;
 import java.nio.ByteBuffer;
-
-import org.apache.maven.shared.io.TestUtils;
 
 import junit.framework.TestCase;
 
 public class FileLocationTest
     extends TestCase
 {
-    
+
     public void testShouldConstructWithFileThenRetrieveSameFile() throws IOException
     {
         File file = File.createTempFile( "test.", ".file-location" );
         file.deleteOnExit();
-        
+
         FileLocation location = new FileLocation( file, file.getAbsolutePath() );
-        
+
         assertSame( file, location.getFile() );
         assertEquals( file.getAbsolutePath(), location.getSpecification() );
     }
-    
+
     public void testShouldReadFileContentsUsingByteBuffer() throws IOException
     {
         File file = File.createTempFile( "test.", ".file-location" );
         file.deleteOnExit();
-        
+
         String testStr = "This is a test";
-        
+
         TestUtils.writeToFile( file, testStr );
-        
+
         FileLocation location = new FileLocation( file, file.getAbsolutePath() );
-        
+
         location.open();
-        
+
         ByteBuffer buffer = ByteBuffer.allocate( testStr.length() );
         location.read( buffer );
-        
+
         assertEquals( testStr, new String( buffer.array() ) );
     }
-    
+
+    public void testShouldReadFileContentsUsingStream() throws IOException
+    {
+        File file = File.createTempFile( "test.", ".file-location" );
+        file.deleteOnExit();
+
+        String testStr = "This is a test";
+
+        TestUtils.writeToFile( file, testStr );
+
+        FileLocation location = new FileLocation( file, file.getAbsolutePath() );
+
+        location.open();
+
+        InputStream stream = location.getInputStream();
+        StringWriter writer = new StringWriter();
+        IOUtil.copy( stream, writer );
+
+        assertEquals( testStr, writer.toString() );
+    }
+
     public void testShouldReadFileContentsUsingByteArray() throws IOException
     {
         File file = File.createTempFile( "test.", ".file-location" );
         file.deleteOnExit();
-        
+
         String testStr = "This is a test";
-        
+
         TestUtils.writeToFile( file, testStr );
-        
+
         FileLocation location = new FileLocation( file, file.getAbsolutePath() );
-        
+
         location.open();
-        
+
         byte[] buffer = new byte[ testStr.length() ];
         location.read( buffer );
-        
+
         assertEquals( testStr, new String( buffer ) );
     }
-    
+
     public void testShouldReadThenClose() throws IOException
     {
         File file = File.createTempFile( "test.", ".file-location" );
         file.deleteOnExit();
-        
+
         String testStr = "This is a test";
-        
+
         TestUtils.writeToFile( file, testStr );
-        
+
         FileLocation location = new FileLocation( file, file.getAbsolutePath() );
-        
+
         location.open();
-        
+
         byte[] buffer = new byte[ testStr.length() ];
         location.read( buffer );
-        
+
         assertEquals( testStr, new String( buffer ) );
-        
+
         location.close();
     }
-    
+
     public void testShouldOpenThenFailToSetFile() throws IOException
     {
         File file = File.createTempFile( "test.", ".file-location" );
         file.deleteOnExit();
-        
+
         TestFileLocation location = new TestFileLocation( file.getAbsolutePath() );
-        
+
         location.open();
-        
+
         try
         {
             location.setFile( file );
-            
+
             fail( "should not succeed." );
         }
         catch( IllegalStateException e )
         {
         }
     }
-    
+
     public void testShouldConstructWithoutFileThenSetFileThenOpen() throws IOException
     {
         File file = File.createTempFile( "test.", ".file-location" );
         file.deleteOnExit();
-        
+
         TestFileLocation location = new TestFileLocation( file.getAbsolutePath() );
-        
+
         location.setFile( file );
         location.open();
     }
-    
+
     public void testShouldConstructWithLocationThenRetrieveEquivalentFile() throws IOException
     {
         File file = File.createTempFile( "test.", ".file-location" );
         file.deleteOnExit();
-        
+
         Location location = new TestFileLocation( file.getAbsolutePath() );
-        
+
         assertEquals( file, location.getFile() );
         assertEquals( file.getAbsolutePath(), location.getSpecification() );
     }
-    
+
     private static final class TestFileLocation extends FileLocation
     {
 
@@ -131,7 +154,7 @@ public class FileLocationTest
         {
             super( specification );
         }
-        
+
     }
 
 }

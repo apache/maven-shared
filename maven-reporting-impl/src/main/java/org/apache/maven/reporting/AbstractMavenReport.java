b08f770892629cf42f8dcb29cba0818f5c5f5bf1
Vincent Siveton <vsiveton@apache.org>
2007-09-28 13:27:58 +0000

    o bumped to doxia's release 1.0-alpha-9 o bumped to maven-reporting-api:2.0.8-SNAPSHOT o updated javadoc o updated license header
    git-svn-id: https://svn.apache.org/repos/asf/maven/shared/trunk@580346 13f79535-47bb-0310-9956-ffa450edef68

--- maven-reporting-impl/src/main/java/org/apache/maven/reporting/AbstractMavenReport.java
+++ maven-reporting-impl/src/main/java/org/apache/maven/reporting/AbstractMavenReport.java
@@ -1,19 +1,22 @@
 package org.apache.maven.reporting;
 
 /*
- * Copyright 2001-2005 The Apache Software Foundation.
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *  http://www.apache.org/licenses/LICENSE-2.0
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 import org.apache.maven.doxia.sink.Sink;
@@ -31,27 +34,24 @@ import java.util.Locale;
  *
  * @author <a href="evenisse@apache.org">Emmanuel Venisse</a>
  * @version $Id$
+ * @since 2.0
  */
 public abstract class AbstractMavenReport
     extends AbstractMojo
     implements MavenReport
 {
+    /** The current sink to use */
     private Sink sink;
 
+    /** The sink factory to use */
     private SinkFactory sinkFactory;
 
-    protected abstract Renderer getSiteRenderer();
-
-    protected abstract String getOutputDirectory();
-
-    protected abstract MavenProject getProject();
-
+    /** The current report output directory to use */
     private File reportOutputDirectory;
 
     /**
-     * TODO: This method is never called - all reports are rendered by maven-site-plugin's
-     * ReportDocumentRender.
-     *
+     * This method should be never called - all reports are rendered by Maven site-plugin's
+     * @see org.apache.maven.plugins.site.ReportDocumentRender
      * @see org.apache.maven.plugin.Mojo#execute()
      */
     public void execute()
@@ -61,31 +61,42 @@ public abstract class AbstractMavenReport
     }
 
     /**
-     * @see org.apache.maven.reporting.MavenReport#generate(org.apache.maven.doxia.sink.Sink, org.apache.maven.reporting.SinkFactory, java.util.Locale)
-     * @deprecated
+     * Generate a report.
+     *
+     * @param aSink the sink to use for the generation.
+     * @param aLocale the wanted locale to generate the report, could be null.
+     * @throws MavenReportException if any
+     * @deprecated using {@link #generate(Sink, SinkFactory, Locale)} instead of
      */
-    public void generate( org.codehaus.doxia.sink.Sink sink, Locale locale )
+    public void generate( org.codehaus.doxia.sink.Sink aSink, Locale aLocale )
         throws MavenReportException
     {
-        getLog().warn( "Deprecated API called - no SinkFactory available. Please update this plugin." );
-        generate( sink, null, locale );
+        getLog().warn( "Deprecated API called - not org.apache.maven.doxia.sink.Sink instance and no SinkFactory available. Please update this plugin." );
+        generate( aSink, null, aLocale );
     }
 
     /**
-     * @see org.apache.maven.reporting.MavenReport#generate(org.apache.maven.doxia.sink.Sink, org.apache.maven.reporting.SinkFactory, java.util.Locale)
-     * @deprecated
+     * Generate a report.
+     *
+     * @see org.apache.maven.reporting.MavenReport#generate(org.apache.maven.doxia.sink.Sink, java.util.Locale)
+     * @deprecated using {@link #generate(Sink, SinkFactory, Locale)} instead of
      */
-    public void generate( Sink sink, Locale locale )
+    public void generate( Sink aSink, Locale aLocale )
         throws MavenReportException
     {
         getLog().warn( "Deprecated API called - no SinkFactory available. Please update this plugin." );
-        generate( sink, null, locale );
+        generate( aSink, null, aLocale );
     }
 
     /**
-     * @see org.apache.maven.reporting.MavenReport#generate(org.apache.maven.doxia.sink.Sink, org.apache.maven.reporting.SinkFactory, java.util.Locale)
+     * Generate a report.
+     *
+     * @param aSink
+     * @param aSinkFactory
+     * @param aLocale
+     * @throws MavenReportException
      */
-    public void generate( Sink sink, SinkFactory sinkFactory, Locale locale )
+    public void generate( Sink aSink, SinkFactory aSinkFactory, Locale aLocale )
         throws MavenReportException
     {
         if ( sink == null )
@@ -99,58 +110,98 @@ public abstract class AbstractMavenReport
             return;
         }
 
-        this.sink = sink;
+        this.sink = aSink;
 
-        this.sinkFactory = sinkFactory;
-        
-        executeReport( locale );
+        this.sinkFactory = aSinkFactory;
+
+        executeReport( aLocale );
 
         closeReport();
     }
 
-    protected abstract void executeReport( Locale locale )
-        throws MavenReportException;
-
-    protected void closeReport()
-    {
-    }
-
+    /** {@inheritDoc} */
     public String getCategoryName()
     {
         return CATEGORY_PROJECT_REPORTS;
     }
 
+    /** {@inheritDoc} */
     public File getReportOutputDirectory()
     {
         if ( reportOutputDirectory == null )
         {
             reportOutputDirectory = new File( getOutputDirectory() );
         }
+
         return reportOutputDirectory;
     }
 
+    /** {@inheritDoc} */
     public void setReportOutputDirectory( File reportOutputDirectory )
     {
         this.reportOutputDirectory = reportOutputDirectory;
     }
 
+    /**
+     * Actions when closing the report. By default, nothing to do.
+     */
+    protected void closeReport()
+    {
+        // nop
+    }
+
+    /**
+     * @return the sink used
+     */
     public Sink getSink()
     {
         return sink;
     }
 
+    /**
+     * @return the sink factory used
+     */
     public SinkFactory getSinkFactory()
     {
         return sinkFactory;
     }
 
+    /**
+     * @see org.apache.maven.reporting.MavenReport#isExternalReport()
+     * @return <tt>false</tt> by default.
+     */
     public boolean isExternalReport()
     {
         return false;
     }
 
+    /** {@inheritDoc} */
     public boolean canGenerateReport()
     {
         return true;
     }
+
+    /**
+     * @return the site renderer used.
+     */
+    protected abstract Renderer getSiteRenderer();
+
+    /**
+     * @return the output directory path.
+     */
+    protected abstract String getOutputDirectory();
+
+    /**
+     * @return the Maven project instance.
+     */
+    protected abstract MavenProject getProject();
+
+    /**
+     * Execute the generation of the report.
+     *
+     * @param locale the wanted locale to return the report's description, could be null.
+     * @throws MavenReportException if any
+     */
+    protected abstract void executeReport( Locale locale )
+        throws MavenReportException;
 }

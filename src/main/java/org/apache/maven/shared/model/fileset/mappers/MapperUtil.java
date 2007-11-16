fea51549c013d04269bd2aa6d4d75d57d4b0c2e2
Vincent Siveton <vsiveton@apache.org>
2007-11-16 00:01:38 +0000

    o added new license header o no code change
    git-svn-id: https://svn.apache.org/repos/asf/maven/shared/trunk/file-management@595499 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/maven/shared/model/fileset/mappers/MapperUtil.java
+++ src/main/java/org/apache/maven/shared/model/fileset/mappers/MapperUtil.java
@@ -1,21 +1,23 @@
-/*
- * Copyright  2000-2004 The Apache Software Foundation
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *
- */
-
 package org.apache.maven.shared.model.fileset.mappers;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 
 import org.apache.maven.shared.model.fileset.Mapper;
 import org.codehaus.plexus.util.IOUtil;
@@ -31,7 +33,7 @@ public final class MapperUtil
 {
 
     private static final String MAPPER_PROPERTIES = "mapper.properties";
-    
+
     private static Properties implementations;
 
     private MapperUtil()
@@ -46,20 +48,20 @@ public final class MapperUtil
         if ( implementations == null )
         {
             Properties props = new Properties();
-            
+
             ClassLoader cloader = Thread.currentThread().getContextClassLoader();
-            
+
             InputStream stream = null;
-            
+
             try
             {
                 stream = cloader.getResourceAsStream( MAPPER_PROPERTIES );
-                
+
                 if ( stream == null )
                 {
                     throw new IllegalStateException( "Cannot find classpath resource: " + MAPPER_PROPERTIES );
                 }
-                
+
                 try
                 {
                     props.load( stream );
@@ -86,12 +88,12 @@ public final class MapperUtil
         {
             return null;
         }
-        
+
         initializeBuiltIns();
-        
+
         String type = mapper.getType();
         String classname = mapper.getClassname();
-        
+
         if ( type == null && classname == null )
         {
             throw new MapperException( "nested mapper or " + "one of the attributes type or classname is required" );

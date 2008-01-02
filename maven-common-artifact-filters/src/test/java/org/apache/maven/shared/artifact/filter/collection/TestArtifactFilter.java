120f9e058484ec9f86549d9b5c18907e6491522b
Brian E Fox <brianf@apache.org>
2008-01-02 04:01:32 +0000

    imported artifact filters from maven-dependency-plugin
    git-svn-id: https://svn.apache.org/repos/asf/maven/shared/trunk@607996 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-common-artifact-filters/src/test/java/org/apache/maven/shared/artifact/filter/collection/TestArtifactFilter.java
@@ -0,0 +1,84 @@
+package org.apache.maven.shared.artifact.filter.collection;
+
+/* 
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+
+/**
+ * 
+ */
+
+import java.util.Iterator;
+import java.util.Set;
+
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.plugin.testing.ArtifactStubFactory;
+
+/**
+ * @author clove TestCases for ArtifactFilter
+ * @see org.apache.maven.plugin.dependency.testUtils.AbstractArtifactFeatureFilterTestCase
+ */
+public class TestArtifactFilter
+    extends AbstractArtifactFeatureFilterTestCase
+{
+
+    protected void setUp()
+        throws Exception
+    {
+        super.setUp();
+        filterClass = ArtifactIdFilter.class;
+        ArtifactStubFactory factory = new ArtifactStubFactory( null, false );
+        artifacts = factory.getArtifactArtifacts();
+    }
+
+    public void testParsing()
+        throws Exception
+    {
+        parsing();
+    }
+
+    public void testFiltering()
+        throws Exception
+    {
+        Set result = filtering();
+        Iterator iter = result.iterator();
+        while ( iter.hasNext() )
+        {
+            Artifact artifact = (Artifact) iter.next();
+            assertTrue( artifact.getArtifactId().equals( "two" ) );
+        }
+    }
+
+    public void testFiltering2()
+        throws Exception
+    {
+        Set result = filtering2();
+        Iterator iter = result.iterator();
+        while ( iter.hasNext() )
+        {
+            Artifact artifact = (Artifact) iter.next();
+            assertTrue( artifact.getArtifactId().equals( "two" ) || artifact.getArtifactId().equals( "four" ) );
+        }
+    }
+
+    public void testFiltering3()
+        throws Exception
+    {
+        filtering3();
+    }
+}

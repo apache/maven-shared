4239bf5274fa6696caa89570de7f5552f8003610
Olivier Lamy <olamy@apache.org>
2008-07-21 23:25:27 +0000

    use plexus-interpolation in the maven-filtering component
    git-svn-id: https://svn.apache.org/repos/asf/maven/sandbox/trunk/shared/maven-filtering@678595 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/main/java/org/apache/maven/shared/filtering/PropertiesEscapingBackSlahValueSource.java
@@ -0,0 +1,57 @@
+package org.apache.maven.shared.filtering;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.util.Properties;
+
+import org.codehaus.plexus.interpolation.ValueSource;
+
+/**
+ * @author <a href="mailto:olamy@apache.org">olamy</a>
+ * @since 21 juil. 2008
+ * @version $Id$
+ */
+public class PropertiesEscapingBackSlahValueSource
+    implements ValueSource
+{
+
+    private boolean escapedBackslashesInFilePath;
+    
+    private Properties properties;
+    
+    /**
+     * 
+     */
+    public PropertiesEscapingBackSlahValueSource(boolean escapedBackslashesInFilePath, Properties properties)
+    {
+        this.escapedBackslashesInFilePath = escapedBackslashesInFilePath;
+        this.properties = properties == null ? new Properties() : properties;
+    }
+
+    /** 
+     * @see org.codehaus.plexus.interpolation.ValueSource#getValue(java.lang.String)
+     */
+    public Object getValue( String expression )
+    {
+        String value = properties.getProperty( expression );
+        return escapedBackslashesInFilePath ? FilteringUtils.escapeWindowsPath( value ) : value;
+    }
+
+}

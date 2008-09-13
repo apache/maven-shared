10600aefe10612817dcba55d3f0ffd05513672d0
Olivier Lamy <olamy@apache.org>
2008-09-13 21:33:06 +0000

    [MSHARED-67] Add a parameter to force overwrite existing files even if the destination files are newer
    git-svn-id: https://svn.apache.org/repos/asf/maven/shared/trunk/maven-filtering@695031 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/java/org/apache/maven/shared/filtering/DefaultMavenFileFilterTest.java
@@ -0,0 +1,94 @@
+package org.apache.maven.shared.filtering;
+
+import java.io.File;
+import java.util.Properties;
+
+import org.codehaus.plexus.PlexusTestCase;
+import org.codehaus.plexus.util.FileUtils;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+/**
+ * @author <a href="mailto:olamy@apache.org">olamy</a>
+ * @version $Id$
+ */
+public class DefaultMavenFileFilterTest
+    extends PlexusTestCase
+{
+    
+    File to = new File(getBasedir(), "target/reflection-test.properties");
+    
+    protected void setUp()
+        throws Exception
+    {
+        super.setUp();
+        if (to.exists())
+        {
+            FileUtils.forceDelete( to );
+        }
+    }
+
+    public void testNotOverwriteFile()
+        throws Exception
+    {
+        MavenFileFilter mavenFileFilter = (MavenFileFilter) lookup( MavenFileFilter.class.getName(), "default" );
+
+        File from = new File( getBasedir(), "src/test/units-files/reflection-test.properties" );
+
+        mavenFileFilter.copyFile( from, to, false, null, null );
+
+        from = new File( getBasedir(), "src/test/units-files/reflection-test-older.properties" );
+
+        // very old file :-)
+        from.setLastModified( 1 );
+
+        to.setLastModified( System.currentTimeMillis() );
+
+        mavenFileFilter.copyFile( from, to, false, null, null );
+
+        Properties properties = PropertyUtils.loadPropertyFile( to, null );
+        assertEquals( "${pom.version}", properties.getProperty( "version" ) );
+
+    }
+    
+    public void testOverwriteFile()
+        throws Exception
+    {
+        MavenFileFilter mavenFileFilter = (MavenFileFilter) lookup( MavenFileFilter.class.getName(), "default" );
+
+        File from = new File( getBasedir(), "src/test/units-files/reflection-test.properties" );
+
+        mavenFileFilter.copyFile( from, to, false, null, null );
+
+        from = new File( getBasedir(), "src/test/units-files/reflection-test-older.properties" );
+
+        // very old file :-)
+        from.setLastModified( 1 );
+
+        to.setLastModified( System.currentTimeMillis() );
+
+        mavenFileFilter.copyFile( from, to, false, null, null, true );
+
+        Properties properties = PropertyUtils.loadPropertyFile( to, null );
+        assertEquals( "older file", properties.getProperty( "version" ) );
+
+    }    
+
+}

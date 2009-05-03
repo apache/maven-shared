d57e8d3bc3b79fca931fd2f6a04160c385e9f8d8
Herve Boutemy <hboutemy@apache.org>
2009-05-03 21:21:29 +0000

    [MPIR-136] avoid quote handling in a link (merged from trunk r771127)
    git-svn-id: https://svn.apache.org/repos/asf/maven/shared/branches/maven-reporting-impl-2.0.4.x@771128 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/maven/reporting/AbstractMavenReportRenderer.java
+++ src/main/java/org/apache/maven/reporting/AbstractMavenReportRenderer.java
@@ -693,7 +693,7 @@ public abstract class AbstractMavenReportRenderer
         {
             char ch = text.charAt( i );
 
-            if ( ch == '\'' && !inQuote )
+            if ( ch == '\'' && !inQuote && braceStack == 0 )
             {
                 // handle: ''
                 if ( i + 1 < text.length() && text.charAt( i + 1 ) == '\'' )

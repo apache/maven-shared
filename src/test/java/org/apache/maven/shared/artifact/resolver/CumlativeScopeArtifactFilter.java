68d81218c9253848a09f0a520d36f5742f5cff57
John Dennis Casey <jdcasey@apache.org>
2009-08-27 16:00:44 +0000

    [MRRESOURCES-41][MSHARED-126] Creating an API based on that found in Maven 3.x (trunk) to resolve the dependency transitive closure for a project or collection of projects. Still needs tests (working on these now).
    NOTE: The package specifications for the two interfaces is critical, since we're aiming to be able to filter out this library in Maven 3.x and simply allow the user to use the built-in functionality instead.
    
    git-svn-id: https://svn.apache.org/repos/asf/maven/shared/trunk/maven-artifact-resolver@808474 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/java/org/apache/maven/shared/artifact/resolver/CumlativeScopeArtifactFilter.java
@@ -0,0 +1,14 @@
+package org.apache.maven.shared.artifact.resolver;
+
+import org.junit.Test;
+
+public class CumlativeScopeArtifactFilter
+{
+    
+    @Test
+    public void testNothingEnabledWhenNoScopesAdded()
+    {
+        
+    }
+
+}

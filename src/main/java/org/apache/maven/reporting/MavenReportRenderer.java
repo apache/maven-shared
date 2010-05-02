4c5815143d309c55d66abedb5d864d6a3e9dc91e
Herve Boutemy <hboutemy@apache.org>
2010-05-02 12:21:33 +0000

    merged back modifications done in Maven-2 trunk
    git-svn-id: https://svn.apache.org/repos/asf/maven/shared/trunk/maven-reporting-api@940199 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/maven/reporting/MavenReportRenderer.java
+++ src/main/java/org/apache/maven/reporting/MavenReportRenderer.java
@@ -20,14 +20,25 @@ package org.apache.maven.reporting;
  */
 
 /**
- * @author Jason van Zyl
+ * Basis for rendering report.
+ *
+ * @author <a href="mailto:jason@maven.org">Jason van Zyl</a>
  * @author <a href="evenisse@apache.org">Emmanuel Venisse</a>
  * @version $Id$
- * @todo Later it may be appropriate to create something like a VelocityMavenReportRenderer that could take a velocity template and pipe that through Doxia rather than coding them up like this.
+ * @since 2.0
+ * @TODO Later it may be appropriate to create something like a VelocityMavenReportRenderer
+ * that could take a velocity template and pipe that through Doxia rather than coding
+ * them up like this.
  */
 public interface MavenReportRenderer
 {
+    /**
+     * @return the wanted report's title.
+     */
     String getTitle();
 
+    /**
+     * Renderer a report.
+     */
     void render();
 }

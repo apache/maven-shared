4ea10d1cddd02f0bc9b24531735e8ac0a098ac7c
Herve Boutemy <hboutemy@apache.org>
2011-04-11 22:15:41 +0000

    fixed headers
    git-svn-id: https://svn.apache.org/repos/asf/maven/shared/trunk@1091231 13f79535-47bb-0310-9956-ffa450edef68

--- maven-osgi/src/main/java/org/apache/maven/shared/osgi/Maven2OsgiConverter.java
+++ maven-osgi/src/main/java/org/apache/maven/shared/osgi/Maven2OsgiConverter.java
@@ -1,3 +1,5 @@
+package org.apache.maven.shared.osgi;
+
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -16,7 +18,6 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.apache.maven.shared.osgi;
 
 import org.apache.maven.artifact.Artifact;
 

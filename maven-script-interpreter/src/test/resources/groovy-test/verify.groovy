3a439fdb1d7ca485b78e14bcd2fe0d5039678d3d
Olivier Lamy <olamy@apache.org>
2011-11-12 21:57:23 +0000

    add a more on default global variables
    git-svn-id: https://svn.apache.org/repos/asf/maven/shared/trunk@1201338 13f79535-47bb-0310-9956-ffa450edef68

--- maven-script-interpreter/src/test/resources/groovy-test/verify.groovy
+++ maven-script-interpreter/src/test/resources/groovy-test/verify.groovy
@@ -1,3 +1,15 @@
+if ( !( basedir instanceof File ) )
+{
+    println "Global script variable not defined: basedir or not a File"
+    throw new RuntimeException("Global script variable not defined: basedir or not a File");
+}
 assert (new File( basedir, "verify.groovy" ).exists())
+
+if ( !( context instanceof Map ) )
+{
+    println "Global script variable not defined: context or not a Map"
+    throw new RuntimeException("Global script variable not defined: context or not a Map");
+}
+
 System.out.println("foo="+context.get("foo"));
 return true

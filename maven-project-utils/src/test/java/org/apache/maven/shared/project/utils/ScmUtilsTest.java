451b8626aa6760eeb780c97e0be94667d135c997
Robert Scholte <rfscholte@apache.org>
2013-04-25 20:00:22 +0000

    Add unit tests
    git-svn-id: https://svn.apache.org/repos/asf/maven/shared/trunk@1475935 13f79535-47bb-0310-9956-ffa450edef68

--- maven-project-utils/src/test/java/org/apache/maven/shared/project/utils/ScmUtilsTest.java
+++ maven-project-utils/src/test/java/org/apache/maven/shared/project/utils/ScmUtilsTest.java
@@ -2,6 +2,8 @@ package org.apache.maven.shared.project.utils;
 
 import static org.junit.Assert.*;
 
+import org.apache.maven.model.Model;
+import org.apache.maven.model.Scm;
 import org.apache.maven.project.MavenProject;
 import org.junit.Test;
 
@@ -20,6 +22,54 @@ public class ScmUtilsTest
     {
         MavenProject project = new MavenProject();
         assertEquals( "",  ScmUtils.resolveScmDeveloperConnection( project ) );
+    }
+
+    @Test
+    public void getScmConnectionByModel()
+    {
+        Model model = new Model();
+        assertNull( ScmUtils.getScmConnection( model ) );
+        model.setScm( new Scm() );
+        assertNull( ScmUtils.getScmConnection( model ) );
+        String connection = "scmConnection";
+        model.getScm().setConnection( connection );
+        assertEquals( connection, ScmUtils.getScmConnection( model ) );
+    }
+
+    @Test
+    public void getScmDeveloperConnectionByModel()
+    {
+        Model model = new Model();
+        assertNull( ScmUtils.getScmDeveloperConnection( model ) );
+        model.setScm( new Scm() );
+        assertNull( ScmUtils.getScmDeveloperConnection( model ) );
+        String connection = "scmConnection";
+        model.getScm().setDeveloperConnection( connection );
+        assertEquals( connection, ScmUtils.getScmDeveloperConnection( model ) );
+    }
+
+    @Test
+    public void getScmConnectionByMavenProject()
+    {
+        MavenProject project = new MavenProject();
+        assertNull( ScmUtils.getScmConnection( project ) );
+        project.setScm( new Scm() );
+        assertNull( ScmUtils.getScmConnection( project ) );
+        String connection = "scmConnection";
+        project.getScm().setConnection( connection );
+        assertEquals( connection, ScmUtils.getScmConnection( project ) );
+    }
+
+    @Test
+    public void getScmDeveloperConnectionByMavenProject()
+    {
+        MavenProject project = new MavenProject();
+        assertNull( ScmUtils.getScmDeveloperConnection( project ) );
+        project.setScm( new Scm() );
+        assertNull( ScmUtils.getScmDeveloperConnection( project ) );
+        String connection = "scmConnection";
+        project.getScm().setDeveloperConnection( connection );
+        assertEquals( connection, ScmUtils.getScmDeveloperConnection( project ) );
     }
 
 }

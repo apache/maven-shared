de3f09612048a518fc74b8aae158ec8ba55ad60d
Herve Boutemy <hboutemy@apache.org>
2014-05-23 22:04:49 +0000

    fixed typo
    git-svn-id: https://svn.apache.org/repos/asf/maven/shared/trunk@1597201 13f79535-47bb-0310-9956-ffa450edef68

--- maven-dependency-tree/src/main/java/org/apache/maven/shared/dependency/graph/internal/Maven3DependencyGraphBuilder.java
+++ maven-dependency-tree/src/main/java/org/apache/maven/shared/dependency/graph/internal/Maven3DependencyGraphBuilder.java
@@ -132,11 +132,11 @@ public class Maven3DependencyGraphBuilder
 
             if ( !result.getUnresolvedDependencies().isEmpty() )
             {
-                throw new DependencyGraphBuilderException( "Could not resolve the following dependencies : "
+                throw new DependencyGraphBuilderException( "Could not resolve the following dependencies: "
                     + result.getUnresolvedDependencies(), e );
             }
 
-            getLogger().debug( "Resolved dependencies after ignoring reactor dependencies : " + reactorDeps );
+            getLogger().debug( "Resolved dependencies after ignoring reactor dependencies: " + reactorDeps );
 
             return result;
         }

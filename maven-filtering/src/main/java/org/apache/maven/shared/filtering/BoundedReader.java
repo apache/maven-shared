ace62e2446e14d675eca8fb71c9e994a03d2839c
Kristian Rosenvold <krosenvold@apache.org>
2014-10-15 17:59:14 +0000

    [MSHARED-325] Fixed bug, fix by me.
    git-svn-id: https://svn.apache.org/repos/asf/maven/shared/trunk@1632145 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-filtering/src/main/java/org/apache/maven/shared/filtering/BoundedReader.java
@@ -0,0 +1,76 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.maven.shared.filtering;
+
+import java.io.IOException;
+import java.io.Reader;
+
+/**
+ * A reader that imposes a limit to the number of bytes that can be read from
+ * an underlying reader, simulating eof when this limit is reached.
+ *
+ * This stream can typically be used to constrain a client with regard to a readAheadLimit
+ * of an underlying stream, to avoid overrunning this limit and hence
+ * lose the opportunity do to reset.
+ */
+public class BoundedReader extends Reader {
+
+	private final Reader target;
+
+	int pos = 0;
+
+	int readAheadLimit;
+
+	public BoundedReader(Reader target, int readAheadLimit) throws IOException {
+		this.target = target;
+		target.mark(readAheadLimit);
+		this.readAheadLimit = readAheadLimit;
+	}
+
+
+	@Override public void close() throws IOException {
+		target.close();
+	}
+
+	@Override public void reset() throws IOException {
+		pos = 0;
+		target.reset();
+	}
+
+	@Override public void mark(int readAheadLimit) throws IOException {
+		this.readAheadLimit = readAheadLimit;
+		target.mark(readAheadLimit);
+	}
+
+	@Override public int read() throws IOException {
+		if (pos >= readAheadLimit) return -1;
+		pos++;
+		return target.read();
+	}
+
+	@Override public int read(char[] cbuf, int off, int len) throws IOException{
+		int c;
+		for (int i = 0; i < len; i++){
+			c = read();
+			if (c == -1) return i;
+			cbuf[off + i] = (char) c;
+		}
+		return len;
+	}
+}

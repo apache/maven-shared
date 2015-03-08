d92a8a6b43330e81276e08b00aaf19aa03caebe0
Robert Scholte <rfscholte@apache.org>
2015-03-08 10:41:34 +0000

    Introduce maven-artifact-transfer as a replacement for Maven2 ArtifactInstaller and ArtifactDeployer. Supports both Maven 3.0 (Sonatype Aether) and Maven 3.1+(Eclipse Aether)
    git-svn-id: https://svn.apache.org/repos/asf/maven/shared/trunk@1664964 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-artifact-tranfer/src/main/java/org/apache/maven/shared/artifact/deploy/internal/Maven31ArtifactDeployer.java
@@ -0,0 +1,86 @@
+package org.apache.maven.shared.artifact.deploy.internal;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.util.Collection;
+
+import org.apache.maven.project.ProjectBuildingRequest;
+import org.apache.maven.shared.artifact.deploy.ArtifactDeployer;
+import org.apache.maven.shared.artifact.deploy.ArtifactDeployerException;
+import org.codehaus.plexus.component.annotations.Component;
+import org.codehaus.plexus.component.annotations.Requirement;
+import org.eclipse.aether.RepositorySystemSession;
+import org.eclipse.aether.artifact.Artifact;
+import org.eclipse.aether.artifact.DefaultArtifact;
+import org.eclipse.aether.deployment.DeployRequest;
+import org.eclipse.aether.deployment.DeploymentException;
+import org.eclipse.aether.impl.Deployer;
+import org.eclipse.aether.metadata.DefaultMetadata;
+import org.eclipse.aether.metadata.Metadata;
+import org.eclipse.aether.metadata.Metadata.Nature;
+
+@Component( role = ArtifactDeployer.class , hint="maven31" )
+public class Maven31ArtifactDeployer implements ArtifactDeployer
+{
+
+    @Requirement
+    private Deployer deployer;
+    
+    public void deploy( ProjectBuildingRequest buildingRequest,
+                         Collection<org.apache.maven.artifact.Artifact> mavenArtifacts )
+        throws ArtifactDeployerException
+    {
+        // prepare request
+        DeployRequest request = new DeployRequest();
+        
+        // transform artifacts
+        for ( org.apache.maven.artifact.Artifact mavenArtifact : mavenArtifacts )
+        {
+            Artifact aetherArtifact =
+                new DefaultArtifact( mavenArtifact.getGroupId(), mavenArtifact.getArtifactId(),
+                                     mavenArtifact.getClassifier(), mavenArtifact.getArtifactHandler().getExtension(),
+                                     mavenArtifact.getVersion(), null, mavenArtifact.getFile() );
+            request.addArtifact( aetherArtifact );
+            
+            if ( mavenArtifact.getMetadataList() != null )
+            {
+                for( org.apache.maven.artifact.metadata.ArtifactMetadata metadata : mavenArtifact.getMetadataList() )
+                {
+                    Metadata aetherMetadata = new DefaultMetadata( metadata.getGroupId(), metadata.getArtifactId(), "maven-metadata.xml", Nature.RELEASE_OR_SNAPSHOT );
+                    
+                    request.addMetadata( aetherMetadata );
+                }
+            }
+        }
+
+        RepositorySystemSession session =
+            (RepositorySystemSession) Invoker.invoke( buildingRequest, "getRepositorySession" );
+
+        // deploy
+        try
+        {
+            deployer.deploy( session, request );
+        }
+        catch ( DeploymentException e )
+        {
+            throw new ArtifactDeployerException( e.getMessage(), e );
+        }
+    }
+}
---
+++ maven-artifact-tranfer/src/main/java/org/apache/maven/shared/artifact/install/internal/Maven30ArtifactInstaller.java
@@ -0,0 +1,88 @@
+package org.apache.maven.shared.artifact.install.internal;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import java.util.Collection;
+
+import org.apache.maven.project.ProjectBuildingRequest;
+import org.apache.maven.shared.artifact.install.ArtifactInstaller;
+import org.apache.maven.shared.artifact.install.ArtifactInstallerException;
+import org.codehaus.plexus.component.annotations.Component;
+import org.codehaus.plexus.component.annotations.Requirement;
+import org.sonatype.aether.RepositorySystemSession;
+import org.sonatype.aether.artifact.Artifact;
+import org.sonatype.aether.impl.Installer;
+import org.sonatype.aether.installation.InstallRequest;
+import org.sonatype.aether.installation.InstallationException;
+import org.sonatype.aether.metadata.Metadata;
+import org.sonatype.aether.metadata.Metadata.Nature;
+import org.sonatype.aether.util.artifact.DefaultArtifact;
+import org.sonatype.aether.util.metadata.DefaultMetadata;
+
+@Component( role = ArtifactInstaller.class, hint = "maven3" )
+public class Maven30ArtifactInstaller
+    implements ArtifactInstaller
+{
+
+    @Requirement
+    private Installer installer;
+
+    public void install( ProjectBuildingRequest buildingRequest,
+                         Collection<org.apache.maven.artifact.Artifact> mavenArtifacts )
+        throws ArtifactInstallerException
+    {
+        // prepare installRequest
+        InstallRequest request = new InstallRequest();
+        
+        // transform artifacts
+        for ( org.apache.maven.artifact.Artifact mavenArtifact : mavenArtifacts )
+        {
+            Artifact aetherArtifact =
+                new DefaultArtifact( mavenArtifact.getGroupId(), mavenArtifact.getArtifactId(),
+                                     mavenArtifact.getClassifier(), mavenArtifact.getArtifactHandler().getExtension(),
+                                     mavenArtifact.getVersion(), null, mavenArtifact.getFile() );
+            
+            request.addArtifact( aetherArtifact );
+            
+            if ( mavenArtifact.getMetadataList() != null )
+            {
+                for( org.apache.maven.artifact.metadata.ArtifactMetadata metadata : mavenArtifact.getMetadataList() )
+                {
+                    Metadata aetherMetadata = new DefaultMetadata( metadata.getGroupId(), metadata.getArtifactId(), "maven-metadata.xml", Nature.RELEASE_OR_SNAPSHOT );
+                    
+                    request.addMetadata( aetherMetadata );
+                }
+            }
+        }
+        
+        RepositorySystemSession session =
+            (RepositorySystemSession) Invoker.invoke( buildingRequest, "getRepositorySession" );
+
+        // install
+        try
+        {
+            installer.install( session, request );
+        }
+        catch ( InstallationException e )
+        {
+            throw new ArtifactInstallerException( e.getMessage(), e );
+        }
+    }
+}

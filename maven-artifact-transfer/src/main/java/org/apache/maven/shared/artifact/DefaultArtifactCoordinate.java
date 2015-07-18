29864f7a3534040693873fce911d01c6f5f08bd1
Robert Scholte <rfscholte@apache.org>
2015-07-18 11:00:28 +0000

    Make ArtifactCoordinate an interface, so other Objects can simply implement it. E.g. o.a.m.p.dependency.fromConfiguration.ArtifactItem needs some extra fields, but it already has all the methods of ArtifactCoordinate. Now we can simple pass ArtifactItem to the resolver without assembling a new Object.
    git-svn-id: https://svn.apache.org/repos/asf/maven/shared/trunk@1691715 13f79535-47bb-0310-9956-ffa450edef68

---
+++ maven-artifact-transfer/src/main/java/org/apache/maven/shared/artifact/DefaultArtifactCoordinate.java
@@ -0,0 +1,151 @@
+package org.apache.maven.shared.artifact;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+/**
+ * Common usage of an ArtifactCoordinate for a Mojo
+ * 
+ * <pre>
+ * &#64;Parameter
+ * private DefaultArtifactCoordinate[] artifacts;
+ * </pre>
+ * 
+ * and
+ * 
+ * <pre>
+ * private DefaultArtifactCoordinate artifact = new DefaultArtifactCoordinate();
+ * 
+ * &#64;Parameter( property = "groupId" )
+ * private String groupId;
+ * 
+ * &#64;Parameter( property = "artifactId" )
+ * private String artifactId;
+ * 
+ * &#64;Parameter( property = "version" )
+ * private String version;
+ * 
+ * &#64;Parameter( property = "classifier" )
+ * private String classifier;
+ * 
+ * &#64;Parameter( property = "type" )
+ * private String type;
+ * 
+ * public void setGroupId( String groupId )
+ * {
+ *   this.artifact.setGroupId( groupId );
+ * }
+ * 
+ * public void setArtifactId( String artifactId )
+ * {
+ *   this.artifact.setArtifactId( artifactId );
+ * }
+ * 
+ * public void setVersion( String version )
+ * {
+ *   this.artifact.setVersion( version );
+ * }
+ * 
+ * public void setClassifier( String classifier )
+ * {
+ *   this.artifact.setClassifier( classifier );
+ * }
+ * 
+ * public void setType( String type )
+ * {
+ *   this.artifact.setType( type );
+ * }
+ * </pre>
+ * 
+ * <strong>Note: </strong> type is not the same as extension! 
+ * {@link org.apache.maven.artifact.handler.ArtifactHandler}s are used to map a type to an extension.  
+ * 
+ * 
+ * @author Robert Scholte
+ * @since 3.0
+ */
+public class DefaultArtifactCoordinate implements ArtifactCoordinate
+{
+    private String groupId;
+    
+    private String artifactId;
+    
+    private String version;
+    
+    private String type;
+    
+    private String classifier;
+    
+    @Override
+    public final String getGroupId()
+    {
+        return groupId;
+    }
+
+    public final void setGroupId( String groupId )
+    {
+        this.groupId = groupId;
+    }
+
+    @Override
+    public final String getArtifactId()
+    {
+        return artifactId;
+    }
+
+    public final void setArtifactId( String artifactId )
+    {
+        this.artifactId = artifactId;
+    }
+
+    @Override
+    public final String getVersion()
+    {
+        return version;
+    }
+
+    public final void setVersion( String version )
+    {
+        this.version = version;
+    }
+
+    @Override
+    public final String getType()
+    {
+        return type != null ? type : "jar";
+    }
+
+    public final void setType( String type )
+    {
+        this.type = type;
+    }
+
+    @Override
+    public final String getClassifier()
+    {
+        return classifier;
+    }
+
+    public final void setClassifier( String classifier )
+    {
+        this.classifier = classifier;
+    }
+    
+    
+}

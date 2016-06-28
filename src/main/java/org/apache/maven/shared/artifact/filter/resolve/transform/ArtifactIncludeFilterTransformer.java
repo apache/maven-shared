ffbd2cf3b37ab3fb74790439891b4c76af5c0c67
Robert Scholte <rfscholte@apache.org>
2016-06-28 20:48:03 +0000

    [MSHARED-566] Support actTransitivily for Pattern(Include/Exclude)ArtifactFilter
    git-svn-id: https://svn.apache.org/repos/asf/maven/shared/trunk/maven-common-artifact-filters@1750584 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/maven/shared/artifact/filter/resolve/transform/ArtifactIncludeFilterTransformer.java
+++ src/main/java/org/apache/maven/shared/artifact/filter/resolve/transform/ArtifactIncludeFilterTransformer.java
@@ -52,17 +52,34 @@ public class ArtifactIncludeFilterTransformer implements FilterTransformer<Artif
     
     private boolean includeNullScope = true;
     
+    private boolean actTransitivelyPattern = false; 
+    
     /**
      * Used by {@link #transform(ScopeFilter)}
      * 
      * When filtering on artifacts it is possible that the scope is unknown.
-     * Decide if artifact should be included is its scoop is {@code null}, default is {@code true}
+     * Decide if artifact should be included if its scope is {@code null}, default is {@code true}
      * 
      * @param includeNullScope set to {@code false} if {@code null}-scoped Artifacts should not be included
      */
     public void setIncludeNullScope( boolean includeNullScope )
     {
         this.includeNullScope = includeNullScope;
+    }
+    
+    /**
+     * Used by {@link #transform(PatternExclusionsFilter)} and {@link #transform(PatternInclusionsFilter)} Determines
+     * whether the include/exclude patterns will be applied to the transitive path of a given artifact. If {@code true},
+     * and the current artifact is a transitive dependency brought in by another artifact which matches an inclusion or
+     * exclusion pattern, then the current artifact has the same inclusion/exclusion logic applied to it as well.
+     * Default is {@code false}
+     * 
+     * @param actTransitivelyPattern set to {@code true} if this artifact should be included/excluded just like one of
+     *            its ancestors.
+     */
+    public void setActTransitivelyPattern( boolean actTransitivelyPattern )
+    {
+        this.actTransitivelyPattern = actTransitivelyPattern;
     }
 
     @Override
@@ -154,13 +171,13 @@ public class ArtifactIncludeFilterTransformer implements FilterTransformer<Artif
     @Override
     public ArtifactFilter transform( PatternExclusionsFilter patternExclusionsFilter )
     {
-        return new PatternExcludesArtifactFilter( patternExclusionsFilter.getExcludes() );
+        return new PatternExcludesArtifactFilter( patternExclusionsFilter.getExcludes(), actTransitivelyPattern );
     }
 
     @Override
     public ArtifactFilter transform( PatternInclusionsFilter patternInclusionsFilter )
     {
-        return new PatternIncludesArtifactFilter( patternInclusionsFilter.getIncludes() );
+        return new PatternIncludesArtifactFilter( patternInclusionsFilter.getIncludes(), actTransitivelyPattern );
     }
 
     @Override

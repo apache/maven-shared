ed39f8993db3e98c566cff17bf163d15cc3342b3
Robert Scholte <rfscholte@apache.org>
2017-02-18 12:43:16 +0000

    [MSHARED-617] (FreeBSD) StreamFeeder.java should flush OutputStream Add unittest
    git-svn-id: https://svn.apache.org/repos/asf/maven/shared/branches/maven-shared-utils-0.9.x@1783526 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/java/org/apache/maven/shared/utils/cli/StreamFeederTest.java
@@ -0,0 +1,53 @@
+package org.apache.maven.shared.utils.cli;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import org.junit.Test;
+
+public class StreamFeederTest
+{
+
+    /**
+     * [MSHARED-617] (FreeBSD) StreamFeeder.java should flush OutputStream
+     * 
+     * @throws IOException
+     */
+    @Test
+    public void testRun() throws IOException
+    {
+        InputStream input = mock( InputStream.class);
+        when( input.read() ).thenReturn( 0, -1 );
+        OutputStream output = mock(OutputStream.class);
+        StreamFeeder feeder = new StreamFeeder( input, output );
+        
+        feeder.run();
+        
+        verify( output ).flush();
+    }
+
+}

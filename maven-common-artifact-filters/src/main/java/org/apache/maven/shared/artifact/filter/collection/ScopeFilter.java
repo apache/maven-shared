09af49e45a3dd509e4ea772849f199da0fa11318
choss <clayton.hoss@googlemail.com>
2017-04-20 12:15:32 +0200

    [MSHARED-631] Changed the scopeFilter to use a LinkedHashSet
    ScopeFilter now uses a LinkedHashSet, so that the
    dependency:build-classpath plugin/target can now preserve the order of the
    classpath entries also while filtering

--- maven-common-artifact-filters/src/main/java/org/apache/maven/shared/artifact/filter/collection/ScopeFilter.java
+++ maven-common-artifact-filters/src/main/java/org/apache/maven/shared/artifact/filter/collection/ScopeFilter.java
@@ -19,7 +19,7 @@ package org.apache.maven.shared.artifact.filter.collection;
  * under the License.    
  */
 
-import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 import org.apache.maven.artifact.Artifact;
@@ -73,7 +73,7 @@ public class ScopeFilter
                 throw new ArtifactFilterException( "Invalid Scope in includeScope: " + includeScope );
             }
 
-            results = new HashSet<Artifact>();
+            results = new LinkedHashSet<Artifact>();
 
             if ( Artifact.SCOPE_PROVIDED.equals( includeScope ) || Artifact.SCOPE_SYSTEM.equals( includeScope ) )
             {
